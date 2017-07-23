41b67e21b498af73c4055a53449bd04aa4055faf
monzo94 <monzo_94@hotmail.com>
2017-07-23 19:58:58 +0200

    Add bullet life timer

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+export var lifeTime = 2 # Time after which the bullet disappears
 export var bounces = false # If true, will bounce on collision with environment instead of exploding
 export var speed = 500 # Speed of the bullet in pixels/s
 var velocity
@@ -7,6 +8,8 @@ var velocity
 
 func _ready():
 	set_fixed_process(true)
+	get_node("timer").set_wait_time(lifeTime)
+	get_node("timer").start()
 	
 func _fixed_process(delta):
 	move(velocity * delta)
@@ -17,3 +20,6 @@ func _fixed_process(delta):
 			velocity -= 2 * velocity.dot(normal) * normal
 		else:
 			queue_free()
+
+func _on_timer_timeout():
+	queue_free()
