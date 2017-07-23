13feddb0d28636374b7e8373ccdd85f7331e2a7c
monzo94 <monzo_94@hotmail.com>
2017-07-23 15:03:18 +0200

    Add comments

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,7 +5,7 @@ const DASH_BOOST = 4 # Speed multiplier for the initial dash speed
 const DASH_DECAY = 1.15 # Rate at which the velocity decays after dashing
 const DASH_THRESHOLD = 1 # Speed at which the dash is considered over
 
-export var speed = 300
+export var speed = 300 # Normal movement speed in pixels/s
 var velocity = Vector2()
 var dashTimer = DASH_CD
 var dashing = false
@@ -14,13 +14,14 @@ func _ready():
 	set_fixed_process(true)
 	
 func _fixed_process(delta):
+	# If in the middle of a dash, decelerate until velocity reaches threshold
 	if dashing:
 		move(velocity)
 		velocity /= DASH_DECAY
 		if velocity.length() <= DASH_THRESHOLD:
 			dashing = false
 	else:
-		# Handle user input
+		# Handle user input and set velocity
 		velocity = Vector2()
 		if (Input.is_action_pressed("move_up")):
 			velocity.y -= 1
@@ -32,6 +33,7 @@ func _fixed_process(delta):
 			velocity.x += 1
 		velocity = velocity.normalized() * speed * delta
 		
+		# If the player can dash, give it a velocity boost and set it to dashing mode
 		if (Input.is_action_pressed("dash") and not dashing and dashTimer >= DASH_CD):
 			dashing = true
 			dashTimer = 0
