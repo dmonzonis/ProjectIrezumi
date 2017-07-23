e8f1c34adb8503db00efde92fe86dbd322fdaa10
monzo94 <monzo_94@hotmail.com>
2017-07-23 14:30:33 +0200

    Add basic dash

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,21 +1,41 @@
 extends KinematicBody2D
 
+const DASH_CD = 1 # Cooldown in seconds for the dash
+const DASH_BOOST = 4 # Speed multiplier for the initial dash speed
+const DASH_DECAY = 1.15 # Rate at which the velocity decays after dashing
+const DASH_THRESHOLD = 1 # Speed at which the dash is considered over
+
 export var speed = 300
 var velocity = Vector2()
+var dashTimer = DASH_CD
+var dashing = false
 
 func _ready():
 	set_fixed_process(true)
 	
 func _fixed_process(delta):
-	velocity = Vector2(0, 0)
-	if (Input.is_action_pressed("move_up")):
-		velocity.y -= 1
-	if (Input.is_action_pressed("move_down")):
-		velocity.y += 1
-	if (Input.is_action_pressed("move_left")):
-		velocity.x -= 1
-	if (Input.is_action_pressed("move_right")):
-		velocity.x += 1
-	velocity = velocity.normalized() * speed * delta
+	if dashing:
+		move(velocity)
+		velocity /= DASH_DECAY
+		if velocity.length() <= DASH_THRESHOLD:
+			dashing = false
+	else:
+		# Handle user input
+		velocity = Vector2()
+		if (Input.is_action_pressed("move_up")):
+			velocity.y -= 1
+		if (Input.is_action_pressed("move_down")):
+			velocity.y += 1
+		if (Input.is_action_pressed("move_left")):
+			velocity.x -= 1
+		if (Input.is_action_pressed("move_right")):
+			velocity.x += 1
+		velocity = velocity.normalized() * speed * delta
+		
+		if (Input.is_action_pressed("dash") and not dashing and dashTimer >= DASH_CD):
+			dashing = true
+			dashTimer = 0
+			velocity = velocity.normalized() * speed * delta * DASH_BOOST
+		elif dashTimer < DASH_CD:
+			dashTimer += delta
 	move(velocity)
-
