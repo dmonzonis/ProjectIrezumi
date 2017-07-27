374d9852528f3fe22bb08b4894b55db1c85d5e88
monzo94 <monzo_94@hotmail.com>
2017-07-27 22:44:47 +0200

    Add basic attacking

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,10 +5,16 @@ const DASH_BOOST = 4 # Speed multiplier for the initial dash speed
 const DASH_DECAY = 1.15 # Rate at which the velocity decays after dashing
 const DASH_THRESHOLD = 1 # Speed at which the dash is considered over
 
+const MELEE_OFFSET = 50 # Distance of the melee collider with the attacker
+const MELEE_CD = 0.2 # Cooldown in seconds for the melee attacking
+
 export var speed = 300 # Normal movement speed in pixels/s
 var velocity = Vector2()
 var dashTimer = DASH_CD
 var dashing = false
+var attacking = true
+var attackTimer = MELEE_CD
+var targetsInRange = []
 
 func _ready():
 	set_fixed_process(true)
@@ -23,13 +29,13 @@ func _fixed_process(delta):
 	else:
 		# Handle user input and set velocity
 		velocity = Vector2()
-		if (Input.is_action_pressed("move_up")):
+		if Input.is_action_pressed("move_up"):
 			velocity.y -= 1
-		if (Input.is_action_pressed("move_down")):
+		if Input.is_action_pressed("move_down"):
 			velocity.y += 1
-		if (Input.is_action_pressed("move_left")):
+		if Input.is_action_pressed("move_left"):
 			velocity.x -= 1
-		if (Input.is_action_pressed("move_right")):
+		if Input.is_action_pressed("move_right"):
 			velocity.x += 1
 		velocity = velocity.normalized() * speed * delta
 		
@@ -40,4 +46,25 @@ func _fixed_process(delta):
 			velocity *= DASH_BOOST
 		elif dashTimer < DASH_CD:
 			dashTimer += delta
+			
+		# Attacking
+		if Input.is_action_pressed("attack") and not attacking and attackTimer >= MELEE_CD:
+			var direction = (get_global_mouse_pos() - get_pos()).normalized()
+			get_node("attackArea").set_pos(direction * MELEE_OFFSET)
+			attacking = true
+		elif attacking:
+			for target in targetsInRange:
+				if target.is_in_group("Enemy"):
+					print("Enemy hit!")
+			attacking = false
+			attackTimer = 0
+		else:
+			attackTimer += delta
+			
 	move(velocity)
+
+func _on_attackArea_body_enter(body):
+	targetsInRange.append(body)
+
+func _on_attackArea_body_exit(body):
+	targetsInRange.erase(body)
