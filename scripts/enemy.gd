c55efa7a072a192edd9b19939eb64ae53c8df793
monzo94 <monzo_94@hotmail.com>
2017-07-28 15:41:33 +0200

    Make enemy follow player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,12 +1,20 @@
 extends KinematicBody2D
 
 export var maxHealth = 5
+export var speed = 200
 var health
 
 func _ready():
 	health = maxHealth
 	add_to_group("Enemy")
-
+	set_fixed_process(true)
+	
+func _fixed_process(delta):
+	var player = get_parent().get_node("player")
+	var direction = (player.get_pos() - get_pos()).normalized()
+	var velocity = direction * speed * delta
+	move(velocity)
+	
 func damage(amount):
 	health -= amount
 	if health <= 0:
